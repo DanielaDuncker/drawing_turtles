3c1e0db05e17530d155be762d334ad9c59bc9549
Daniela Duncker <Daniela@Daniela-MacBook-Pro.local>
2019-03-21 10:21:37 +0200

    repetitive version

--- drawing_turtles.py
+++ drawing_turtles.py
@@ -1,1 +1,34 @@
-print("hello")
+import turtle
+
+
+def draw_square():
+    window = turtle.Screen()
+    window.bgcolor("green")
+
+    tim = turtle.Turtle()
+    tim.shape("turtle")
+    tim.color("orange")
+    tim.speed(1)
+
+    tim.forward(100)
+    tim.right(90)
+    tim.forward(100)
+    tim.right(90)
+    tim.forward(100)
+    tim.right(90)
+    tim.forward(100)
+    tim.right(90)
+
+    nick = turtle.Turtle()
+    nick.shape("turtle")
+    nick.color("purple")
+
+    nick.circle(100)
+
+    window.exitonclick()
+
+
+draw_square()
+
+# shapes: “arrow”, “turtle”, “circle”, “square”, “triangle”, “classic”
+# speed: fastest: 0, fast: 10, normal: 6, slow: 3, slowest: 1
