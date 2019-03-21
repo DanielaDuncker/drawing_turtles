651f8997e0ea5fb94fb1254bd87715b120b8fd7a
Daniela Duncker <Daniela@Daniela-MacBook-Pro.local>
2019-03-21 13:10:59 +0200

    comment not working parts

--- drawing_turtles.py
+++ drawing_turtles.py
@@ -1,53 +1,53 @@
 import turtle
 
 
-#def draw_square(joe_turtle):
-#    for i in range(1, 5):
-#        joe_turtle.forward(100)
-#        joe_turtle.right(90)
+def draw_square(joe_turtle):
+    for i in range(1, 5):
+        joe_turtle.forward(100)
+        joe_turtle.right(90)
 
 
-#def draw_circle(nick_turtle):
-#    for i in range(1):
-#        nick_turtle.circle(50)
-
-
-def draw_triangle(kevin_turtle):
+def draw_circle(nick_turtle):
     for i in range(1):
-        kevin_turtle.forward(100)
-        kevin_turtle.right(120)
+        nick_turtle.circle(50)
+
+
+# def draw_triangle(kevin_turtle):
+#    for i in range(1):
+#        kevin_turtle.forward(100)
+#        kevin_turtle.right(120)
 
 
 def draw_art():
     window = turtle.Screen()
     window.bgcolor("green")
 
-#    joe = turtle.Turtle()
-#    joe.shape("turtle")
-#    joe.color("orange")
-#    joe.speed(0)
-
-#    for i in range(1, 37):
-#        draw_square(joe)
-#        joe.right(10)
-
-#    nick = turtle.Turtle()
-#    nick.shape("turtle")
-#    nick.color("purple")
-#    nick.speed(0)
-
-#    for i in range(1, 37):
-#        draw_circle(nick)
-#        nick.right(10)
-
-    kevin = turtle.Turtle()
-    kevin.shape("turtle")
-    kevin.color("red")
-    kevin.speed(2)
+    joe = turtle.Turtle()
+    joe.shape("turtle")
+    joe.color("orange")
+    joe.speed(0)
 
     for i in range(1, 37):
-        draw_triangle(kevin)
-        kevin.right(10)
+        draw_square(joe)
+        joe.right(10)
+
+    nick = turtle.Turtle()
+    nick.shape("turtle")
+    nick.color("purple")
+    nick.speed(0)
+
+    for i in range(1, 37):
+        draw_circle(nick)
+        nick.right(10)
+
+    #    kevin = turtle.Turtle()
+    #    kevin.shape("turtle")
+    #    kevin.color("red")
+    #    kevin.speed(10)
+
+    #    for i in range(1, 37):
+    #        draw_triangle(kevin)
+    #        kevin.right(20)
 
     window.exitonclick()
 
